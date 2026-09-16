c225b350d0911bf87be96c2e1fd6ecdabc788de9
darrelyang436 <darrelyang436@users.noreply.github.com>
2026-09-16 16:15:32 +0800

    Add package sources and postinstall.

--- test/abi.test.js
+++ test/abi.test.js
@@ -2,13 +2,9 @@
 
 const test = require("node:test");
 const assert = require("node:assert/strict");
-const abi = require("../src");
+const meta = require("../src");
 
-test("encode/decode address", () => {
-  const addr = "0x0000000000000000000000000000000000000001";
-  assert.equal(abi.decodeAddress(abi.encodeAddress(addr)), addr);
-});
-
-test("encode uint256", () => {
-  assert.equal(abi.encodeUint256(1n).endsWith("1"), true);
+test("chainName", () => {
+  assert.equal(meta.chainName(1), "ethereum");
+  assert.equal(meta.isMainnet(1), true);
 });
